83e2de5c77bb74010370fcb2e59d4438d6192447
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-05-24 14:38:40 -0300

    incluindo funcao editar tarefa e atualizando em local storage

--- script-crud.js
+++ script-crud.js
@@ -7,6 +7,10 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
@@ -26,6 +30,13 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
+     
+    botao.onclick = () => {
+        const novaDescricao = prompt("qual é o nome da nova tarefa")
+        paragrafo.textContent = novaDescricao ;
+        tarefa.descricao = novaDescricao;
+        atualizarTarefas ();
+    }
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/edit.png')
@@ -50,7 +61,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas ()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
